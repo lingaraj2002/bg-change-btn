c0407b8da64bfffb7edaa3c742c8f8d302d709a9
lingaraj <119045348+lingaraj2002@users.noreply.github.com>
2023-02-28 10:45:25 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let color = ["red","blue","green","yellow","aqua","orange","olive","gray"];
 let body = document.querySelector("body");
 let button1 = document.querySelector("#btn");
 
+
 button1.addEventListener("click",function(){	
 	body.style.backgroundColor = color[Math.round(Math.random()*color.length)];	
 })
